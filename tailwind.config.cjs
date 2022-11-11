afe850aa7008027511f9a2a6bd65a30b0b247264
Michael Grenier <michael@grenier.dev>
2022-11-10 20:47:49 -0500

    feat: blog scaffolding

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,md,mdx,js,jsx,ts,tsx}"],
+	theme: {
+		extend: {}
+	},
+	darkMode: 'class',
+	plugins: []
+};
